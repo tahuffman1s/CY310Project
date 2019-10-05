bb94d77523e26e15d618703f77cc1c7d36bd5774
Travis Huffman <travis@pop-os.localdomain>
2019-10-05 01:56:44 -0500

    Fixes to compilation script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # This script compiles the code
-g++ -std=c++17 tomcrypt.h strm.h aes.c strm.cpp main.cpp -o project 
+g++ -std=c++17 tomcrypt_custom.h tomcrypt.h strm.h aes.c strm.cpp main.cpp -o project 
