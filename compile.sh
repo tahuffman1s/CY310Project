1ecf875394a0923ad67313cf1ceba88170bc349a
Travis Huffman <travis@pop-os.localdomain>
2019-10-05 01:25:38 -0500

    Got rid of unecessary flag

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # This script compiles the code
-g++ -std=c++17 strm.h strm.cpp main.cpp -o project -lncurses -ltinfo
+g++ -std=c++17 strm.h strm.cpp main.cpp -o project -ltinfo
